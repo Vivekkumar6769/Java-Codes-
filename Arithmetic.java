c1c5676a93ea36729bf42559b9aa8a542893f136
Vivek Kumar <85937313+Vivekkumar6769@users.noreply.github.com>
2022-10-02 15:46:40 +0530

    Add files via upload

---
+++ Arithmetic.java
@@ -0,0 +1,20 @@
+package chap2;
+
+public class Arithmetic 
+{
+	public static void main(String[] args) 
+	{
+	/* int x=10;
+	int y=20;
+	int sum =x++ + ++y + x++ + y++ + x + y ;
+	System.out.println(sum);
+	System.out.println(x +" "+y);
+	System.out.println(y); */
+		int a=3;
+		a=a++;
+		System.out.println(a);
+		a= ++a;
+		System.out.println(a);
+	}
+
+}
